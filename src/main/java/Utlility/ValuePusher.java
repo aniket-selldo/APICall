bdd1aec6ba5e32d7baa7b94b63ca763649d54615
Aniket <Aniket@DESKTOP-NTHEPI3>
2025-04-15 15:58:45 +0530

    Test

---
+++ src/main/java/Utlility/ValuePusher.java
@@ -0,0 +1,51 @@
+package Utlility;
+
+public class ValuePusher {
+
+	private String value_1;
+	private String value_2;
+	private String value_3;
+	private String value_4;
+	private String value_5;
+
+	public String getValue_1() {
+		return value_1;
+	}
+
+	public void setValue_1(String value_1) {
+		this.value_1 = value_1;
+	}
+
+	public String getValue_2() {
+		return value_2;
+	}
+
+	public void setValue_2(String value_2) {
+		this.value_2 = value_2;
+	}
+
+	public String getValue_3() {
+		return value_3;
+	}
+
+	public void setValue_3(String value_3) {
+		this.value_3 = value_3;
+	}
+
+	public String getValue_4() {
+		return value_4;
+	}
+
+	public void setValue_4(String value_4) {
+		this.value_4 = value_4;
+	}
+
+	public String getValue_5() {
+		return value_5;
+	}
+
+	public void setValue_5(String value_5) {
+		this.value_5 = value_5;
+	}
+
+}
--- src/main/java/net/apicall/demo/Controller/SelldoController.java
+++ src/main/java/net/apicall/demo/Controller/SelldoController.java
@@ -4,10 +4,14 @@ import static io.restassured.RestAssured.given;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import Utlility.ConsoleColors;
+import Utlility.ValuePusher;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
 
@@ -19,6 +23,8 @@ public class SelldoController {
 	private static final String CID = "587ddb2b5a9db31da9000002";
 	private static final String website = "fa8d6ca0217e676a7b0e06f51c32568c";
 	private static final String fullAPI = "c13ad8e13264b1c22bc39bb475889c7e";
+	private static final String SRD = "";
+	private static final String Project = "";
 	 
 	@GetMapping("id/{lead}")
 	public String getLead(@PathVariable String lead) {
@@ -34,6 +40,36 @@ public class SelldoController {
 		return given().urlEncodingEnabled(false).contentType(ContentType.JSON).with().queryParam("api_key", fullAPI)
 				.queryParam("client_id", CID).queryParam("value", lead).get(URL).then().parser("text/html", Parser.JSON)
 				.statusCode(200).extract().response().jsonPath().prettyPrint();
+	}
 	
+	@PostMapping("/createlead")
+	public String createLead(@RequestBody ValuePusher je) {
+		
+		String name = je.getValue_1();
+		String email = je.getValue_2();
+		String phone = je.getValue_3();
+		String source = je.getValue_4();
+		String subsource = je.getValue_5();
+		String payload = "{\r\n"
+				+ "    \"sell_do\": {\r\n"
+				+ "        \"form\": {\r\n"
+				+ "            \"lead\": {\r\n"
+				+ "                \"name\": \""+name+"\",\r\n"
+				+ "                \"phone\": \""+phone+"\",\r\n"
+				+ "                \"email\": \""+email+"\",\r\n"
+				+ "                \"sub_source\": \""+subsource+"\",\r\n"
+				+ "                \"source\": \""+source+"\",\r\n"
+				+ "                \"project_id\": \""+Project+"\"\r\n"
+				+ "            }\r\n"
+				+ "        },\r\n"
+				+ "        \"campaign\": {\r\n"
+				+ "             \"srd\": \""+SRD+"\" \r\n"
+				+ "        }\r\n"
+				+ "    },\r\n"
+				+ "    \"api_key\": \""+website+"\"\r\n"
+				+ "}";
+		
+		return RestAssured.given().urlEncodingEnabled(false).contentType(ContentType.JSON).body(payload).when()
+				.post(domain + "/api/leads/create").then().parser("text/html", Parser.JSON).extract().response().jsonPath().getString("sell_do_lead_id");
 	}
 }
